2bccd0197897b3317481d8fe0ec0e3fa095ae8f0
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-03-01 17:11:16 +0900

    build: 프로젝트 생성 및 세팅

---
+++ RxSwift/RxSwift/Application/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  RxSwift
+//
+//  Created by 조성민 on 3/1/25.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+    }
+}
---
+++ RxSwift/RxSwift/Feature/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  RxSwift
+//
+//  Created by 조성민 on 3/1/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
